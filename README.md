d57188068cab6c565b7b7004bad4e87dcbe68e74
keyanap <keyanapregent@gmail.com>
2023-11-27 19:56:20 -0500

    Completed Work and README

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # belly-button-challenge
+
+An interactive dashboard was created to explore the Belly Button Biodiversity dataset (https://robdunnlab.com/projects/belly-button-biodiversity/). 
+
+The dashboard contains a dropdown menu, a demographic panel, a horizontal bar graph, and a bubble chart. 
+
+Dropdown Menu 
+
+The dropdown menu is used to toggle between the visualizations by Test Subject ID No. 
+
+![Alt text](<Screenshot 2023-11-27 at 7.40.35 PM.png>)
+
+Demographic Panel
+
+The demographic panel shows the demographics information of the Test Subject ID. The key-value pairs from the metadata of the JSON object are displayed. 
+
+![Alt text](<Screenshot 2023-11-27 at 7.36.05 PM.png>)
+
+Horizontal Bar Graph
+
+The top ten OTUs found in each Test Subject ID are displayed in a bar graph. sample_values are the x values for the chart and the otu_ids are the y values for the chart. The otu_labels are shown in hovertext in the chart.  
+
+![Alt text](<Screenshot 2023-11-27 at 7.35.42 PM.png>)
+
+Bubble Chart
+
+The bubble chart shows each sample found from each Test Subject ID. otu_ids are the x values for the chart and the sample_values are the y values for the chart. The hovertext for the chart is based off otu_labels and the colours are dictated by otu_ids. 
+
+![Alt text](<Screenshot 2023-11-27 at 7.35.51 PM.png>)
+
+
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,78 +1,124 @@
 // URL is placed in variable 
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
- // Fetch the JSON data and console log it
- d3.json(url).then(function(data) {
-    console.log(data);
- });
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    console.log("Data: ", data);
+});
 
-// Display default plot 
+// Define dropdown menu
+let dropdownMenu = d3.select("#selDataset");
+
+// Display the default plot
 function init() {
-     // Use D3 to select the dropdown menu
-     let dropdownMenu = d3.select("#selDataset");
-
-    // Create an array for sample names
-    let names = data.names;
-
-    // Add sample names to dropdown menu 
-    names.forEach((name) => {
-        dropdownMenu.append("option").text(name).property("value", name);
-    });
-    // Set first sample name
-    let name = names[0];
-
-    // Create functions to make plots
-    buildDemographics(name);
-    buildBar(name);
-    buildBubble(name);
-};
-
-  // Build horizontal bar graph
-  function buildBar(selectedSample) {
-    // Fetch JSON data and console log it
+    // Fetch JSON data
     d3.json(url).then(function(data) {
-        console.log(data);
+        // Store names in list
+        let dropdown = data.names;
+        // Console log it 
+        console.log("Dropdown: ", dropdown);
+        // Add list to dropdown menu
+        dropdown.forEach((id) => {
+            dropdownMenu.append("option").text(id).property("value", id);
+        });
+    // Create functions to make plots
+    buildBar(dropdown);
+    buildDemo(dropdown);
+    buildBubble(dropdown);
+    // Make default option for display
+    let defaultPlot = dropdown[0];
+        console.log("Default Plot: ", defaultPlot);
+    // Create default plots
+    buildBar(defaultPlot);
+    buildDemo(defaultPlot);
+    buildBubble(defaultPlot);
     });
-        // Create array for sample objects
-        let samples = data.samples;
-        // Filter data where selectedSample is id
-        let filteredData = samples.filter((sample) => sample.id == selectedSample);
-        // Assign first object
-        let object = filteredData[0];
-        // Trace data
-        let trace1 = [{
-            // Use slice for top 10
-            x: object.sample_values.slice(0,10).reverse(),
-            y: object.otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
-            text: object.otu_labels.slice(0,10).reverse(),
-            type: "bar",
-            marker: {color: "blue"},
-            orientation: "h"
-        }];
 };
 
-    // Build bubble graph
-    function buildBubble(selectedSample) {
-        // Fetch JSON data and console log it
-        d3.json(url).then(function(data) {
-            console.log(data);
-        });
-        // Create an array for sample objects 
-        let samples = data.samples;
+// Build horizontal bar graph
+function buildBar(selectedSample) {
+    // Fetch JSON data
+    d3.json(url).then((data) => {
+    // Create array for sample objects
+    let samplesData = data.samples;
+    // Filter data where selectedSample is id
+    let filteredData = samplesData.filter(id => id.id == selectedSample);
+    // Assign first object
+    let first = filteredData[0];
+    // Trace data
+    let trace1 = [{
+        // Use slice for top 10
+        x: first.sample_values.slice(0,10).reverse(),
+        y: first.otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
+        text: first.otu_labels.slice(0,10).reverse(),
+        type: "bar",
+        marker: {color: "blue"},
+        orientation: "h"
+    }];
+    let layout = {title: "Top Ten OTUs"};
+    // Use Plotly
+    Plotly.newPlot("bar", trace1, layout);
+    });
+};
+
+// Build demographics panel
+function buildDemo(selectedSample) {
+    // Fetch JSON data 
+    d3.json(url).then((data) => {
+        // Create an array for metadata objects 
+        let demoInfo = data.metadata;
         // Filter data where selectedSample is id
-        let filteredData = samples.filter((sample) => sample.id === selectedSample);
+        let filtered = demoInfo.filter(id => id.id == selectedSample);
         // Assign first object
-        let object = filteredData[0];
-        // Trace data
-        let trace2 = [{
-            x: object.otu_ids,
-            y: object.sample_values,
-            text: object.otu_labels,
-            mode: "markers",
-            marker: {
-                size: object.sample_values,
-                color: object.otu_ids,
-                colorscale: "Sunset"
-            }
-        }];
-    }
+        let firstobj = filtered[0];
+        // Clear metadata
+        d3.select('#sample-metadata').text('');
+        // Use object.entries to add key pairs
+        Object.entries(firstobj).forEach(([key,value]) => {
+            d3.select('#sample-metadata').append('h5').text(`${key}: ${value}`); 
+        });
+    });
+};
+
+// Build bubble graph
+function buildBubble(selectedSample) {
+    // Fetch JSON data
+    d3.json(url).then((data) => {
+    // Create an array for sample objects 
+    let samplesData = data.samples;
+    // Filter data where selectedSample is id
+    let filteredData = samplesData.filter(id => id.id == selectedSample);
+    // Assign first object
+    let firstob = filteredData[0];
+    // Assign first object to variables
+    let sample_values = firstob.sample_values;
+    let otu_labels = firstob.otu_labels;
+    let otu_ids = firstob.otu_ids;
+    // Trace data
+    let trace2 = [{
+        x: otu_ids,
+        y:sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+            size: sample_values,
+            color: otu_ids
+        }
+    }];
+    // Set layout
+    let layout = {
+        xaxis: {title: "OTU ID"},
+        yaxis: {title: "Number of Bacteria"}
+    };
+    // Use Plotly
+    Plotly.newPlot("bubble", trace2, layout);
+});
+};
+
+// Toggle between options
+function optionChanged(selectedSample) {
+    buildBar(selectedSample);
+    buildDemo(selectedSample);
+    buildBubble(selectedSample);
+}
+init();
